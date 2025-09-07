daa4ec712dec414561923d33c9e7559e04b422dd
Elaman Zarlykov <z.elaman699@gmail.com>
2025-09-07 06:06:55 +0600

    feat: add react-query-devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ConverterPage from "./pages/ConverterPage";
+import "./global.scss";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 30000),
+      staleTime: 5 * 60 * 1000, // 5 min
+      gcTime: 10 * 60 * 1000, // 10 min
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: true,
+    },
+  },
+});
 
 function App() {
-  const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
@@ -11,6 +25,7 @@ function App() {
           <Route path="/" element={<ConverterPage />} />
         </Routes>
       </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
